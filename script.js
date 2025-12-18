da92e8531cb3bea2f58ebdfda7a2ea9cf96ea162
digitarod <digitarod@gmail.com>
2025-12-18 23:57:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // Replace this with the deployed Web App URL from Google Apps Script
-    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyEUfEv-EMKpVoZUfEY6AfSWEngnQj6v8iy2WorP6HdgQoNhCtxVqBxXW06w1D8O7CB/exec'; 
+    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbz92q3NJsQ4_0Pdv-6kb5FJmoUUITZt-PeNKmUQyQ42ArOH9_EGqMFLiqvvx05-q-_pxw/exec'; 
     
     // For demo purposes, we will mock the backend response
     const USE_MOCK_BACKEND = false; 
